30a17d3314837c90f14df3bd39f3bd176ee63149
Carlos Espinoza <cespinoza@swc.com.ec>
2021-12-17 02:28:54 -0500

    v1

---
+++ verify/js/main.js
@@ -0,0 +1,21 @@
+﻿let cosplaybo = angular.module('cosplaybo', []);
+
+cosplaybo.controller('VerifierController', ['$scope', async function ($scope) {
+  let urlParams = new URLSearchParams(window.location.search)
+  let hash = urlParams.get('hash')
+
+  $scope.loading = true
+  $scope.data = {}
+
+  if (hash) {
+    let res = await fetch(`http://chimecho.rocks:6500/?hash=${hash}`, {"method": "GET"})
+    res = await res.json()
+
+    $scope.$apply(function () {
+      $scope.data = res
+      $scope.loading = false
+    })
+  } else {
+    window.location.href = '/'
+  }
+}])
