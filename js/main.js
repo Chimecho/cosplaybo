8e85b677a34c79cf4a6be70d19e0975cf2bd8252
Carlos Espinoza <cespinoza@swc.com.ec>
2021-12-16 23:20:29 -0500

    sugar

---
+++ js/main.js
@@ -0,0 +1,31 @@
+﻿cosplaybo = angular.module("cosplaybo", ['ngRoute']);
+
+const debug = true
+
+cosplaybo.config(function($routeProvider, $locationProvider) {
+	$routeProvider
+		.when('/', {
+			templateUrl: 'views/home.html',
+			controller: 'Home',
+			reloadOnSearch: false,
+		})
+		.when('/verify', {
+			templateUrl: 'views/verify.html',
+			controller: 'Verify',
+			reloadOnSearch: false,
+		})
+	;
+});
+
+cosplaybo.controller('Home', ['$scope', function($scope) {}]);
+
+cosplaybo.controller('Verify', ['$scope', '$http', function($scope, $http) {
+	console.log($http)
+}]);
+
+cosplaybo.controller('Main', ['$scope, $router', function($scope, $router) {
+	console.log($router)
+}]);
+
+if (!debug)
+	console.log = function () {}
